e54c0103ac9b11f021666ff0921bfe75240a1e38
Jimmy-Cannon Nwadinkpa <arinze.nwadinkpa@gmail.com>
2019-02-26 22:14:50 +0100

    added flexbox

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const Joi = require('joi');
+const User = require('models/User');
+
 const router = express.Router();
 
+const schema = Joi.object().keys({
+	email: Joi.string().email(),
+	password: Joi.string().regex(/[a-zA-Z0-9!"#€%&"]{6,30}/)
+});
+
 router.get('/register', function(req, res){
-	res.render("register", {
+	res.render('register', {
 		page: {
 			register: true
 		},
@@ -10,8 +18,32 @@ router.get('/register', function(req, res){
 });
 
 router.post('/register', function(req, res) {
-	console.log(req.body);
-	res.render('register-success');
+	const result = Joi.validate(req.body, schema);
+	
+	if (result.error !== null) {
+		// return res.status(400).send(result.error.message);
+		return res.render('register', {
+			page: {
+				register: true
+			},
+			email: req.body.email,
+			error: result.error
+		});
+	}
+
+	User.create(req.body).then(function() {
+		res.render('register-success');
+	}).catch(function(error) {
+		res.render('register', {
+			page: {
+				register: true
+			},
+			email: req.body.email,
+			error
+		}); 
+	});
+
+	
 });
 
-module.exports = router
+module.exports = router;
